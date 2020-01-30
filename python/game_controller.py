5aa30fa2d87e1fec478babd12ae8168471921549
Alexander Pezarro <alexanderpezarro@gmail.com>
2020-01-30 12:59:09 +0200

    Readablity changes
    Readded most of the game code to a game_setup method and condensed the run code to a single method

--- python/game_controller.py
+++ python/game_controller.py
@@ -2,53 +2,70 @@ import characters
 import data
 import random
 
+player_name = ""
+player_char = None
 
-print("\n\n\n\tThe Game!\n")
 
-char_name = input("Please enter your characters name: ")
-
-print(f"Wellcome {char_name}!")
-print("It's time to choose your character, first choose a race.\n")
-
-avalible_races = data.get_playable_races(True)
-
-print("The avalible races are: ", end="")
-
-for i in avalible_races:
-    if i == avalible_races[-2]:
-        print(i, end=" and ")
-    elif i == avalible_races[-1]:
-        print(i, end=". ")
+def game():
+    kill_count = 0
+    player_info = game_setup()
+    player_name, player_char = player_info[0], player_info[1]
+    while player_char.health > 0:
+        reg_round(player_char, player_name)
+        kill_count += 1
     else:
-        print(i, end=", ")
+        return f"\n\tGAME OVER\n\tYou managed to kill {kill_count} enemy(s)\n\tThanks for playing!\n\tCreated by Alexander Pezarro\n"
 
-race_choice = input("Which race would you like? : ").capitalize()
 
-while not race_choice or not data.is_race_valid(race_choice):
-    race_choice = input(
-        "That choice wasn't valid, please try again. Which race would you like? : ").capitalize()
+def game_setup():
 
-print(f"{race_choice}, a good choice! Now to choose a class.")
-print("The avalible classes are: ", end="")
-avalible_classes = data.get_assignable_classes(True)
-for i in avalible_classes:
-    if i == avalible_classes[-2]:
-        print(i[0].capitalize(), end=" and ")
-    elif i == avalible_classes[-1]:
-        print(i[0].capitalize(), end=". ")
-    else:
-        print(i[0].capitalize(), end=", ")
+    print("\n\n\n\tThe Game!\n")
 
-class_choice = input("Which class would you like? : ").lower()
+    char_name = input("Please enter your characters name: ")
 
-while not class_choice or not data.is_class_valid(class_choice):
-    class_choice = input(
-        "That choice wasn't valid, please try again. Which class would you like? : ").lower()
-print(f"A {class_choice.capitalize()}, perfect!")
+    print(f"Wellcome {char_name}!")
+    print("It's time to choose your character, first choose a race.\n")
 
-player_char = characters.get_player(race_choice, class_choice)
+    avalible_races = data.get_playable_races(True)
 
-print(f"Well {char_name} the {player_char.name.lower()}, I wish you well on your adventure, bon voyage!\n")
+    print("The avalible races are: ", end="")
+
+    for i in avalible_races:
+        if i == avalible_races[-2]:
+            print(i, end=" and ")
+        elif i == avalible_races[-1]:
+            print(i, end=". ")
+        else:
+            print(i, end=", ")
+
+    race_choice = input("Which race would you like? : ").capitalize()
+
+    while not race_choice or not data.is_race_valid(race_choice):
+        race_choice = input(
+            "That choice wasn't valid, please try again. Which race would you like? : ").capitalize()
+
+    print(f"{race_choice}, a good choice! Now to choose a class.")
+    print("The avalible classes are: ", end="")
+    avalible_classes = data.get_assignable_classes(True)
+    for i in avalible_classes:
+        if i == avalible_classes[-2]:
+            print(i[0].capitalize(), end=" and ")
+        elif i == avalible_classes[-1]:
+            print(i[0].capitalize(), end=". ")
+        else:
+            print(i[0].capitalize(), end=", ")
+
+    class_choice = input("Which class would you like? : ").lower()
+
+    while not class_choice or not data.is_class_valid(class_choice):
+        class_choice = input(
+            "That choice wasn't valid, please try again. Which class would you like? : ").lower()
+    print(f"A {class_choice.capitalize()}, perfect!")
+
+    player_char = characters.get_player(race_choice, class_choice)
+
+    print(f"Well {char_name} the {player_char.name.lower()}, I wish you well on your adventure, bon voyage!\n")
+    return [char_name, player_char]
 
 
 def player_turn(enemy, player, player_name):
@@ -117,13 +134,13 @@ def reg_round(player, player_name):
         print(player_turn(enemy, player, player_name))
         if enemy.health == 0:
             print(
-                f"Congradulations {player_name} you defeated the {enemy.name}!")
+                f"Congradulations {player_name} you defeated the {enemy.name}!\n")
             break
         else:
             print(enemy_turn(enemy, player))
         if player.health == 0:
-            print(f"{player_name} was defeated by the {enemy.name}")
+            print(f"{player_name} was defeated by the {enemy.name}\n")
             break
 
 
-reg_round(player_char, char_name)
+print(game())
