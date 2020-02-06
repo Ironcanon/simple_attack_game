e0f7f3e98dbf28aa3ff52041a401fca03f6245bf
Alexander Pezarro <alexanderpezarro@gmail.com>
2020-02-06 09:14:59 +0200

    Fixed Typos and change an attack Figured out how to spell desription. Added slam chance to dwarf_slam attack.

--- python/attacks.py
+++ python/attacks.py
@@ -1,3 +1,4 @@
+import random
 attacks = {
     # layout = ["def_name", "ingame_name"]
     0: ["attack_basic", "basic attack"],
@@ -15,7 +16,7 @@ def attack_basic(char, target, check=False):
             target.health = 0
         return(f"\n{char.name} attacked {target.name} for {char.attack} damage!\n{target.name} has {target.health} health left.")
     else:
-        # layout = ["attack_cost_sescription", mana_cost, ammo_cost]
+        # layout = ["attack_cost_description", mana_cost, ammo_cost]
         return [f"This attack will do {char.attack} dammage and is free", 0, 0]
 
 
@@ -31,7 +32,7 @@ def attack_archer_basic(char, target, check=False):
         else:
             return(f"\n{char.name} is out of arrows so the attack failed!")
     else:
-        # layout = ["attack_cost_sescription", mana_cost, ammo_cost]
+        # layout = ["attack_cost_description", mana_cost, ammo_cost]
         return [f"This attack will do {char.attack + 5} dammage and costs 1 ammo", 0, 1]
 
 
@@ -47,23 +48,27 @@ def attack_mage_basic(char, target, check=False):
         else:
             return(f"\n{char.name} is out of mana so the attack failed!")
     else:
-        # layout = ["attack_cost_sescription", mana_cost, ammo_cost]
+        # layout = ["attack_cost_description", mana_cost, ammo_cost]
         return [f"This attack will do {char.attack * 1.5} dammage and costs 5 mana", 5, 0]
 
 
 def attack_dwarf_slam(char, target, check=False):
+    stunned_chance = 0.5
     if not check:
         if char.mana >= 10:
             target.health -= (char.attack * 2)
             target.health = round(target.health, 1)
             char.mana -= 10
+            effect_chance = random.random()
+            if effect_chance < stunned_chance:
+                target.status_effects.append(0)
             if target.health <= 0:
                 target.health = 0
             return(f"\n{char.name} slammed {target.name} for {char.attack * 2} damage!\n{target.name} has {target.health} health left.")
         else:
             return(f"\n{char.name} is out of mana so the attack failed!")
     else:
-        # layout = ["attack_cost_sescription", mana_cost, ammo_cost]
+        # layout = ["attack_cost_description", mana_cost, ammo_cost]
         return [f"This attack will do {char.attack * 2} dammage and costs 10 mana", 10, 0]
 
 
