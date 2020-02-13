08636ffaf4cb6c1c7d70e1f46340e1df9f6cae87
Alexander Pezarro <alexanderpezarro@gmail.com>
2020-02-13 10:28:52 +0200

    Added bandages to dropped items

--- python/items.py
+++ python/items.py
@@ -22,7 +22,7 @@ items = {
     7: ["blue ring", "Seems like an ordinary ring not going to lie", "jewellery", "m+5", 0.5],
     8: ["bone", "An old, flaky bone, just what I wanted for christmas", "rubish", "n", 0.9],
     9: ["rotten flesh", "Eww, just very eww", "rubish", "n", 0.9],
-    10: ["bandage", "It won't do much but it's better than nothing", "once_off_consumable", "h+2", 0.7],
+    10: ["bandage", "It won't do much but it's better than nothing", "once_off_consumable", "H+2", 0.7],
     11: ["health potion", "Not sure what's in here but it sure fixes up wounds", "consumable", "H+20", 0.4],
     12: ["elvish bow", "The elves are know for their archery and even more for their bows", "weapon", ["d+5", "a+5"], 0.2],
     13: ["imp charm", "A carved imp horn tied around your neck, it seems to give off an aura of energy", "jewellery", ["h+5", "m*1.5"], 0.3],
